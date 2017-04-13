aff25c5374e3596ec107328d3c4adc71c799d63c
MennovNoort <mennotoshiba@hotmail.com>
2017-04-13 21:17:00 +0200

    Website eerste upload
    Eerst upload van de website. Nog lang niet af !

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+    <?php include 'partials/menu.php'; ?>
+    
+    <div class="banner-foto">
+        <div class="banner-kleur">
+            <div class="banner-content">
+                <h2>LOREM IPSUM DOLOR SIT AMET</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                <br>
+                <a href="#"><button class="bet-now">Lorem ipsum elit!</button></a>
+            </div>    
+        </div>
+    </div>
+    <div class="container">
+        <h3>What is lorem ipsum?</h3>
+        <p class="homepagina-tekst">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        </p>
+        <br>
+        <br>
+        <h3>Recent bets</h3>
+        <div class="bet-layout">
+            <h3>Ajax - Schalke 04</h3>
+            <p>Bet: Both team to score</p>
+            <p>@Yes</p>
+            <br>
+            <br>
+            <a href="#">More about this bet</a>
+            
+        </div>
+        <div class="bet-layout">
+            
+        </div>
+        <div class="bet-layout">
+            
+        </div>
+    </div>
+    
+    <?php include 'partials/footer.php'; ?>
+</body>
+</html>
---
+++ partials/menu.php
@@ -0,0 +1,11 @@
+<div class="menu-balk">
+    <div class="menu-wrapper">
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="index.php">Over ons</a></li>
+                <li><a href="index.php">Contact</a></li>
+            </ul>
+        </nav>
+    </div>    
+</div>
